970ebe9287d4ac6535ad223cb7bc26e6d89ce2c1
jmwit <jmwit@138.236.179.207>
2019-11-25 09:44:26 -0600

    Added test site. Wrote test class.

--- server/test/server/RequestFileTest.java
+++ server/test/server/RequestFileTest.java
@@ -1,10 +1,35 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class RequestFileTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// establish socket connection
+        Socket s = new Socket("localhost", 80);
+        PrintWriter out = new PrintWriter(s.getOutputStream(), false);
 
+        // make request to socket
+        out.print("GET " + args[0] + " HTTP/1.1\r\n");
+        out.print("Host: localhost");
+        out.print("\r\n");
+        out.flush();
+        
+        // read content
+        InputStreamReader isr = new InputStreamReader(s.getInputStream());
+        BufferedReader br = new BufferedReader(isr);
+        int c;
+        while ((c = br.read()) != -1) {
+          System.out.print((char) c);
+        }
+        
+        // close connection
+        s.close();
 	}
 
 }
